ed437aad67f7a4c7c3e7e292e3300b17d7a1ceb9
Geovane Fedrecheski <geonnave@gmail.com>
2019-02-26 10:19:03 -0300

    PDP can match operations; run formatter

--- lib/ac/pdp.ex
+++ lib/ac/pdp.ex
@@ -3,6 +3,11 @@ defmodule AC.PDP do
     false
   end
 
+  @doc """
+  Tests whether the request attributes are allowed by a policy.
+
+  Returns true if the set `request_ops` is a subset of `policy_ops`.
+  """
   def match_attrs(request_attrs, policy_attrs) do
     policy_attrs
     |> Enum.all?(fn policy_attr ->
@@ -12,5 +17,18 @@ defmodule AC.PDP do
 
   def match_attr(_request_attr = {key, value}, policy_attr) do
     policy_attr.name == key and policy_attr.value == value
+  end
+
+  @doc """
+  Tests whether the request operations are allowed by a policy.
+
+  Returns true if the set `request_ops` is a subset of `policy_ops`.
+  """
+  def match_operations([], _policy_ops), do: false
+  def match_operations(_request_ops, []), do: false
+
+  def match_operations(request_ops, policy_ops) do
+    request_ops
+    |> Enum.all?(&(&1 in policy_ops))
   end
 end
--- test/ac_test.exs
+++ test/ac_test.exs
@@ -1,31 +1,32 @@
 defmodule ACTest do
   use ExUnit.Case
   doctest AC
+  alias AC.{PDP, Attr, Policy}
 
   test "authorize a request" do
     policies = [
-      %AC.Policy{
+      %Policy{
         id: "...",
         name: "Adult Home Control",
         user_attrs: [
-          %AC.Attr{data_type: "string", name: "Type", value: "Person"},
-          %AC.Attr{data_type: "string", name: "Role", value: "FamilyMember"},
-          %AC.Attr{data_type: "range", name: "Age", value: %{min: 18}}
+          %Attr{data_type: "string", name: "Type", value: "Person"},
+          %Attr{data_type: "string", name: "Role", value: "FamilyMember"},
+          %Attr{data_type: "range", name: "Age", value: %{min: 18}}
         ],
         operations: ["all"],
         object_attrs: [
-          %AC.Attr{data_type: "string", name: "Type", value: "AirConditioner"}
+          %Attr{data_type: "string", name: "Type", value: "AirConditioner"}
         ]
       },
-      %AC.Policy{
+      %Policy{
         id: "...",
         name: "Human Home Control",
         user_attrs: [
-          %AC.Attr{data_type: "string", name: "Type", value: "Person"}
+          %Attr{data_type: "string", name: "Type", value: "Person"}
         ],
         operations: ["read"],
         object_attrs: [
-          %AC.Attr{data_type: "string", name: "Type", value: "AirConditioner"}
+          %Attr{data_type: "string", name: "Type", value: "AirConditioner"}
         ]
       }
     ]
@@ -43,14 +44,14 @@ defmodule ACTest do
       operations: ["read"]
     }
 
-    refute AC.PDP.authorize(request)
+    refute PDP.authorize(request)
   end
 
   test "match single request attribute against policy attribute" do
-    policy_attr = %AC.Attr{data_type: "string", name: "Type", value: "Person"}
-    assert AC.PDP.match_attr({"Type", "Person"}, policy_attr)
-    refute AC.PDP.match_attr({"Id", "Person"}, policy_attr)
-    refute AC.PDP.match_attr({"Type", "Camera"}, policy_attr)
+    policy_attr = %Attr{data_type: "string", name: "Type", value: "Person"}
+    assert PDP.match_attr({"Type", "Person"}, policy_attr)
+    refute PDP.match_attr({"Id", "Person"}, policy_attr)
+    refute PDP.match_attr({"Type", "Camera"}, policy_attr)
   end
 
   test "match list of request attributes against list of policy attributes" do
@@ -62,13 +63,23 @@ defmodule ACTest do
     }
 
     policy_attrs = [
-      %AC.Attr{data_type: "string", name: "Type", value: "Person"},
-      %AC.Attr{data_type: "string", name: "Role", value: "FamilyMember"}
+      %Attr{data_type: "string", name: "Type", value: "Person"},
+      %Attr{data_type: "string", name: "Role", value: "FamilyMember"}
     ]
 
-    assert AC.PDP.match_attrs(request_attrs, policy_attrs)
-    refute AC.PDP.match_attrs(Map.delete(request_attrs, "Type"), policy_attrs)
-    refute AC.PDP.match_attrs(Map.delete(request_attrs, "Role"), policy_attrs)
-    assert AC.PDP.match_attrs(Map.delete(request_attrs, "Id"), policy_attrs)
+    assert PDP.match_attrs(request_attrs, policy_attrs)
+    refute PDP.match_attrs(Map.delete(request_attrs, "Type"), policy_attrs)
+    refute PDP.match_attrs(Map.delete(request_attrs, "Role"), policy_attrs)
+    assert PDP.match_attrs(Map.delete(request_attrs, "Id"), policy_attrs)
+  end
+
+  test "match request operations against policy operations" do
+    refute PDP.match_operations([], ["read"])
+    refute PDP.match_operations(["read"], [])
+
+    assert PDP.match_operations(["read"], ["read", "update", "delete"])
+    assert PDP.match_operations(["read", "update"], ["read", "update", "delete"])
+    assert PDP.match_operations(["read", "update", "delete"], ["read", "update", "delete"])
+    refute PDP.match_operations(["create", "read", "update", "delete"], ["read", "update", "delete"])
   end
 end
