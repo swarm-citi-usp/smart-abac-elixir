45301f0aeb99ca28c77a94f9c3eaea035278c178
Geovane Fedrecheski <geonnave@gmail.com>
2020-11-02 10:42:39 -0300

    Bump version to 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ABACthem.MixProject do
   def project do
     [
       app: :abac_them,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
